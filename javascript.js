1fc89b44e9690ceb72056ba3039abafed7a8885d
Sarah Polachek <polacheksarah@gmail.com>
2021-08-04 00:15:36 -0400

    newupdates and pages

--- javascript.js
+++ javascript.js
@@ -16,3 +16,20 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+/*// Get the container element
+var btnContainer = document.getElementById("navbar");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}*/
+
+//form
